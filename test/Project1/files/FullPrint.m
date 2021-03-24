6435fccfa7aba958e182e0d50a14cb0b2cd6216d
JeanSOUDIER <soudier.jean@gmail.com>
2021-03-24 09:08:04 +0100

    modif matlab rates

---
+++ test/Project1/files/FullPrint.m
@@ -0,0 +1,67 @@
+clear all;
+close all;
+clc;
+
+Nfile = '0';
+
+Ttime = table2array(readtable(strcat('Time_',Nfile,'.csv')));
+Data = table2array(readtable(strcat('ePID_points_',Nfile,'.csv')));
+Tsensor = table2array(readtable(strcat('DVS_timing_',Nfile,'.csv')));
+Tcontroler = table2array(readtable(strcat('ePID_timing_',Nfile,'.csv')));
+Dsensor = table2array(readtable(strcat('DVS_points_',Nfile,'.csv')));
+Dtracker = table2array(readtable(strcat('Cluster_points_',Nfile,'.csv')));
+
+Ysensor = zeros(1,Ttime(:,2)-Ttime(:,1));
+TdiffSensor = Tsensor(:,2)-Tsensor(:,1);
+for i = 1:length(Tsensor)
+    Ysensor(:,Tsensor(i,1)-Ttime(:,1):Tsensor(i,1)-Ttime(:,1)+TdiffSensor(i,1)) = ones(1,TdiffSensor(i,1)+1);
+end
+
+Ycontroler = zeros(1,Ttime(:,2)-Ttime(:,1));
+Tdiffconstroler = Tcontroler(:,2)-Tcontroler(:,1);
+for i = 1:length(Tcontroler)
+    Ycontroler(:,Tcontroler(i,1)-Ttime(:,1):Tcontroler(i,1)-Ttime(:,1)+Tdiffconstroler(i,1)) = ones(1,Tdiffconstroler(i,1)+1);
+end
+
+axi = Ttime(:,2)-Ttime(:,1);
+
+figure(1);
+tiledlayout(2,1);
+nexttile;
+hold on;
+plot(Data(:,4)-Ttime(:,1)*ones(length(Data),1),Data(:,1),'or');
+xlim([0 axi]);
+plot(Data(:,4)-Ttime(:,1)*ones(length(Data),1),Data(:,2),'og');
+xlim([0 axi]);
+plot(Data(:,4)-Ttime(:,1)*ones(length(Data),1),Data(:,3),'ob');
+xlim([0 axi]);
+hold off;
+
+nexttile;
+hold on;
+plot(Ysensor);
+axis([0 axi -0.1 1.1]);
+plot(Ycontroler);
+axis([0 axi -0.1 1.1]);
+hold off;
+
+figure(2);
+hold on;
+plot3(Dtracker(:,1),Dtracker(:,2),Dtracker(:,4)-Ttime(:,1)*ones(length(Dtracker),1),'ro');
+plot3(Dsensor(:,1),Dsensor(:,2),Dsensor(:,4)-Ttime(:,1)*ones(length(Dsensor),1),'ko');
+view(3);
+hold off;
+
+loadTracker = sum(Ysensor)/(Dsensor(length(Dsensor),4)-Dsensor(1,4));
+loadController = sum(Ycontroler)/(Dtracker(length(Dtracker),4)-Dtracker(1,4));
+
+fprintf('Tracker load %f %%\n',loadTracker);
+fprintf('Controller load %f %%\n',loadController);
+
+MeanTimeTracker = sum(TdiffSensor)/length(TdiffSensor);
+MaxEventRateTracker = 1/(MeanTimeTracker*1e-3);
+MeanTimeController = sum(Tdiffconstroler)/length(Tdiffconstroler);
+MaxEventRateController = 1/(MeanTimeController*1e-3);
+
+fprintf('Mean time tracker %f us, %f Kevts/s\n',MeanTimeTracker,MaxEventRateTracker);
+fprintf('Mean time controller %f us, %f Kevts/s\n',MeanTimeController,MaxEventRateController);
