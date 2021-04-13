fcfb3344d2b9f0624e0bd5ded83f6cbc53c2d0c5
JeanSOUDIER <soudier.jean@gmail.com>
2021-04-13 11:09:18 +0200

    modif class look_up

--- look_up_table/look_up/main.cpp
+++ look_up_table/look_up/main.cpp
@@ -2,8 +2,12 @@
 #include <fstream>
 #include <string>
 #include <cmath>
+#include <functional>
 
-#define PRINT
+#include "look_up.hpp"
+
+//#define PRINT
+#define DATA_TYPE double
 
 #ifdef PRINT
 #include "look_up_table.hpp"
@@ -11,32 +15,11 @@
 
 int main() {
     #ifndef PRINT
-        std::ofstream file = std::ofstream("look_up_table.hpp");
-        std::string data_type = "float";
-        float lim_min = 0;
-        float lim_max = 80;
-        float epsilon = 0.01;
-
-        file << "const float lim_min = " << static_cast<float>(lim_min) << ", lim_max = " << static_cast<float>(lim_max) << ", epsilon = " << static_cast<float>(1.0f/epsilon) << ";\n";
-        file << "const " << data_type << " expo[] = {\n";
-        double i_last;
-        for(double i=lim_min;i<lim_max-epsilon;i+=epsilon) {
-            if(data_type == "float") {
-                file << "   " << static_cast<float>(std::exp(i)) << ",\n";
-            } else if(data_type == "double") {
-                file << "   " << static_cast<double>(std::exp(i)) << ",\n";
-            }
-            i_last = i;
-        }
-        if(data_type == "float") {
-            file << "   " << static_cast<float>(std::exp(i_last+epsilon)) << "\n};";
-        } else if(data_type == "double") {
-            file << "   " << static_cast<double>(std::exp(i_last+epsilon)) << "\n};";
-        }
-        file.close();
-        std::cout << "done" << std::endl;
+        std::function<DATA_TYPE(DATA_TYPE)> f_exp = [=](const DATA_TYPE& in) {return static_cast<DATA_TYPE>(std::exp(in));};
+        look_up<DATA_TYPE> table("look_up_table", f_exp, 0.005f, 0, 80);
     #else
         std::cout << expo[static_cast<int>(5.0*epsilon)] << std::endl;
     #endif
+
     return 0;
 }
